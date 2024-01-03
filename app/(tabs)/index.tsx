787b34278252ea450536c7fb347404f3639336a0
gracebir <grace.devolop@gmail.com>
2024-01-03 07:43:47 +0200

    router set up

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,11 +2,14 @@
 
 import { View, Text } from "react-native";
 import React from "react";
+import { Link } from "expo-router";
 
 const Page = () => {
     return (
         <View>
-            <Text>Papge</Text>
+            <Link href={"/(modals)/login"}>Login</Link>
+            <Link href={"/(modals)/booking"}>Booking</Link>
+            <Link href={"/listing/134"}>listing</Link>
         </View>
     );
 };
