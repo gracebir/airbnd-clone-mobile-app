14253871f6b42c24bad87fe784c51c8ec4ece45b
gracebir <grace.devolop@gmail.com>
2024-01-03 15:50:25 +0200

    check if the user is login to logout

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,21 @@
 /** @format */
 
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import React from "react";
+import { useAuth } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
 
 const Page = () => {
+    const { isSignedIn, signOut } = useAuth();
     return (
         <View>
-            <Text>profile</Text>
+            {isSignedIn ? (
+                <Button title='Log out' onPress={() => signOut()} />
+            ) : (
+                <Link href={"/(modals)/login"}>
+                    <Text>Login</Text>
+                </Link>
+            )}
         </View>
     );
 };
